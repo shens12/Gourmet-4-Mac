b15da9a93660c64726dcd7727ff74d197c21355d
Lan Wang <wangl139@mcmaster.ca>
2020-11-18 00:05:28 -0500

    For the img to show

--- src/pages/AfterRequestPage.js
+++ src/pages/AfterRequestPage.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { useData } from "../data/useData";
 import { useNavigate, useParams } from "react-router-dom";
 
-export default function ReportSubmittedPage() {
+export default function AfterRequestPage() {
     let navigate = useNavigate();
 
 	return (
 		<div className="group1">
-			<p className="title1">We have received your report. </p>
-			<p className="title2">You will receive feedback as soon as the problem is resolved.</p>
-			<img className="img4" src="../afterReport.jpg" /> <br />
+			<p className="title1">Your service request has been sent to people near this restaurant! </p>
+			<p className="title2">Take a break！Your food is on the way to Mac…</p>
+			<img className="img4" src="./afterService.jpg" /> <br />
 			<button className="button" onClick={() => navigate(`/`)}>Back to main page</button>
+			<br />
 		</div>
     );
 }
