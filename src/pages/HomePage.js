bf390e54eb6ec49677ba4287fa5354da29f5b4df
Lan Wang <wangl139@mcmaster.ca>
2020-11-18 01:08:51 -0500

    Attempt4

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -20,7 +20,7 @@ export default function Homepage() {
 		<div className="group2">
 			<div className="subgroup4">
 				<Link to="/mobile">
-					<img className="img" src="./Delivery.png" alt="Delivery" /> 
+					<img className="img" src="https://shens12.github.io/Gourmet-4-Mac/Delivery.png" alt="Delivery" /> 
 					<p className="clickable"> Delivery for others <img width="20" src="./triangle1.png" /></p>
 					</Link>
 				<p className="desc"> Take ordered dished to campus <br />
